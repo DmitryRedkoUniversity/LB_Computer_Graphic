521d5ffd9a73ae998f6ea76537e0c22213e9d490
dmitry(danitaso) <dimaredko444@gmail.com>
2023-09-16 08:57:59 +0700

    done

--- LB2/script.js
+++ LB2/script.js
@@ -1,12 +1,14 @@
 // Получаем ссылки на кнопки и экран анимации
 const button1 = document.getElementById("button1");
 const button2 = document.getElementById("button2");
+const button3 = document.getElementById("button3");
 const animationScreen = document.getElementById("animation-screen");
 
 // Массив с классами для форм (круг, квадрат, ромб)
 const shapes = ["circle", "square", "diamond"];
 let currentIndex = 0; // Индекс текущей формы
 let isMoving = false; // Флаг для отслеживания движения
+let isMasked = false; // Флаг для отслеживания использования маски
 
 // Обработчик события для кнопки 1 (смена формы)
 button1.addEventListener("click", () => {
@@ -30,6 +32,16 @@ button2.addEventListener("click", () => {
     }
 });
 
+// Обработчик события для кнопки 3 (использование маски)
+button3.addEventListener("click", () => {
+    // Переключаем состояние использования маски
+    if (!isMasked) {
+        applyMask();
+    } else {
+        removeMask();
+    }
+});
+
 // Функция для начала движения фигуры по траектории
 function startMoving() {
     if (!isMoving) {
@@ -43,5 +55,21 @@ function stopMoving() {
     if (isMoving) {
         isMoving = false;
         animationScreen.style.animation = "none"; // Останавливаем анимацию
+    }
+}
+
+// Функция для применения маски к фигуре
+function applyMask() {
+    if (!isMasked) {
+        isMasked = true;
+        animationScreen.style.clipPath = "polygon(50% 0%, 0% 100%, 100% 100%)"; // Пример маски
+    }
+}
+
+// Функция для удаления маски с фигуры
+function removeMask() {
+    if (isMasked) {
+        isMasked = false;
+        animationScreen.style.clipPath = "none"; // Убираем маску
     }
 }
